e271e0d42103f1d22342434a1e10a3a234d4b5fc
basiacarvalho <basiacarvalho@gmail.com>
2025-10-24 14:48:43 +0200

    creating readme file

---
+++ README.md
@@ -0,0 +1,62 @@
+## react-6-worldwise
+
+# 🗺 "Worldwise" 🗺
+
+## Table of contents
+
+- [Project Title](#"Worldwise")
+- [Overview](#overview)
+- [The Project](#the-project)
+- [Technology stack](#technology-stack)
+- [Links](#links)
+- [Visuals](#visuals)
+- [Project Status](#project-status)
+- [Author](#author)
+
+## Overview
+
+### The Project
+
+<p align="justify">The project from 'The Ultimate React Course 2025: React, Next.js, Redux & More' on Udemy by Jonas Schmedtmann.
+</p>
+
+### Technology stack
+
+- HTML
+- CSS
+- JavaScript
+- React
+
+### Links
+
+- Source code repository URL: [Github](https://github.com/basiacarvalho/react-6-react-worlwise)
+- Live Site URL: [Live demo](https://basiacarvalho.github.io/react-6-worlwise/)
+
+### Visuals
+
+#### Desktop view :
+
+![Screenshot](./public/worldwide-photo.png)
+
+### Project Status
+
+- Completed (with the possiblity for future adaption)\
+  The basic concepts of the project have been met, however further ideas may be implemented and added in the future.
+
+### Author
+
+<p align="justify">👩‍💻 The "Worldwise" project is the result of taking part in a course on Udemy: 'The Ultimate React Course 2025: React, Next.js, Redux & More' by Jonas Schmedtmann. The code comes from the course content (The "README.md" file was created by me).
+</p>
+
+### Available Scripts
+
+In the project directory, you run both:
+
+#### `npm run dev`
+
+#### `npm run server`
+
+Runs the app in the development mode.
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.
---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRout from "./pages/ProtectedRoute";
+
+import City from "./components/City";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import Login from "./pages/Login";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
+// dist/assets/index-5e1077cc.css   30.41 kB │ gzip:   5.11 kB
+// dist/assets/index-683695e9.js   512.59 kB │ gzip: 149.23 kB
+
+function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRout>
+                    <AppLayout />
+                  </ProtectedRout>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;

1ba6ccdc7d3cee42c3bfdbe3fea6594ce91de344
basiacarvalho <basiacarvalho@gmail.com>
2025-10-24 16:48:41 +0200

    commenting unnecessary code

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -2,7 +2,7 @@ import { useCallback, useReducer } from "react";
 import { createContext, useEffect, useContext } from "react";
 import citiesData from "../../data/cities.json";
 
-const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
 
 const CitiesContext = createContext();
 
